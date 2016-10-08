a629e13a3096d1218037c2d0fb0f95f7e8cdb6fa
Thomas Bechtold <tbechtold@suse.com>
2016-10-08 23:36:07 +0200

    Use same version for doc and sdist

--- setup.py
+++ setup.py
@@ -19,12 +19,14 @@
 import setuptools
 import metaextract.setup
 
+from metaextract import __version__
+
 with open("README.rst", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="metaextract",
-    version="0.0.8",
+    version=__version__,
     license="Apache-2.0",
     description="get metadata for python modules",
     long_description=long_description,
